d9676b66a5ff67ce20096e4e0a5f085e68dec044
dac <siddhantmalik304@gmail.com>
2023-05-17 09:58:28 +0530

    test

---
+++ db.sql
@@ -0,0 +1,23 @@
+create table customer(
+custid int,
+cname varchar(20),
+address varchar(20),
+adharno long
+);
+
+create table destination(
+trainno int,
+tname varchar(20),
+tdest varchar(20)
+);
+
+create table flight(
+flightno int,
+fname varchar(20),
+fdest varchar(20)
+);
+create table destination(
+Did int,
+dname varchar(20)
+);
+
